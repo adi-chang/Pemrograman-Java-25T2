71d5af319a58ec69c8f686f60001a2e9d78abfd6
adi <adi4vista@gmail.com>
2026-04-02 20:06:58 +0700

    update sample demo program pada pertemuan-12

--- pertemuan-12/Latihan01.java
+++ pertemuan-12/Latihan01.java
@@ -42,6 +42,25 @@ public class Latihan01 {
         return false;
     }
 
+    private static String toTitleCase(String name) {
+        StringBuilder sb = new StringBuilder(name.length());
+        boolean toUpper = true;
+        for ( Character c : name.toCharArray() ) {
+            if ( Character.isSpaceChar(c) ) {
+                toUpper = true;
+            }
+            else if ( toUpper ) {
+                c = Character.toUpperCase(c);
+                toUpper = false;
+            }
+            else {
+                c = Character.toLowerCase(c);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         try ( Scanner sc = new Scanner(System.in) ) {
             
@@ -52,17 +71,46 @@ public class Latihan01 {
                 displayDataItems();
                 System.out.println();
                 System.out.print("Name [ enter to stop ] ? "); name = sc.nextLine();
-                if ( !name.isEmpty() ) {
+                if ( !name.strip().isEmpty() ) {
                     if ( isNameExists(name) ) {
                         // jika sudah ada, reject
                         System.out.println();
                         System.out.println("Sorry, nama yang sama, sudah ada dalam list ...");
-                        System.out.println("Press enter to continue ...");
-                        sc.nextLine();
+                        System.out.println();
+                        System.out.print("Change to name [ enter to cancel ] ? "); 
+                        String name2 = sc.nextLine();
+                        if ( !name2.strip().isEmpty() ) {
+                            if ( name.equalsIgnoreCase(name2) ) {
+                                System.out.println();
+                                System.out.println("Sorry, nama baru tidak boleh sama dengan nama lama ...");
+                                System.out.println("Press enter to continue ...");
+                                sc.nextLine();
+                            }
+                            else if ( isNameExists(name2) ) {
+                                System.out.println();
+                                System.out.println("Sorry, nama yang sama sudah ada ...");
+                                System.out.println("Press enter to continue ...");
+                                sc.nextLine();
+                            }
+                            else {
+                                List<String> lists = Files.readAllLines(Paths.get(filename));
+                                int idx = -1;
+                                for ( int i = 0 ; i < lists.size() ; i++ ) {
+                                    if ( lists.get(i).equalsIgnoreCase(name) ) {
+                                        idx = i;
+                                        break;
+                                    }
+                                }
+                                if ( idx >= 0 ) {
+                                    lists.set(idx, toTitleCase(name2));
+                                    Files.write(Paths.get(filename), lists);
+                                }
+                            }
+                        }
                     }
                     else {
                         // jika merupakan nama baru 
-                        Files.write(Paths.get(filename), "%s\n".formatted(name).getBytes(), 
+                        Files.write(Paths.get(filename), "%s\n".formatted(toTitleCase(name)).getBytes(), 
                                     StandardOpenOption.CREATE, StandardOpenOption.APPEND);
                     }
                 }
